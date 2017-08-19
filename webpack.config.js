0b4bda3179231245c8524b8ef086626f78ef5506
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-19 18:41:29 -0400

    Sass loader.

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,14 @@ module.exports = {
         ]
       },
       {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         options: {
