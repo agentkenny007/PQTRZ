7722323e80884730f15bdf94c3d8de57d9df2147
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-19 08:07:18 -0400

    Refactor. Logic to register, log in and upload.

--- App/modules/backbone.js
+++ App/modules/backbone.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import React from 'react';
 import access_token from './constants/token';
 import cookies from './cookies';
+import proxy from './constants/proxy';
 import requests from './requests';
 
 class Backbone { // the Backbone class
@@ -12,13 +13,13 @@ class Backbone { // the Backbone class
   /* | controls the session.
   /* |
   /* | The Backbone is itself initialised before it is exported. This means that The Back-
-  /* | bone can be use anywhere in the app, within any ES6 module, simply by importing it.
+  /* | bone can be used anywhere in the app, within any ES6 module, simply by importing it.
   /* | No matter where you call it from, the reference is always to the same Backbone ins-
-  /* | ance 'bone' (little bone) constructed at the end of this module.
+  /* | ance 'app' (little app) constructed at the end of this module.
   /* |
-  /* | Little bone can even be called from within this module to control things like apply-
-  /* | ing settings and live DOM insertion. That's because any reference saved to the Back-
-  /* | bone gets attached to little bone. Essentially, little bone runs the app.
+  /* | Little app can even be called from within this class wherever 'this' takes on a dif-
+  /* | ferent context (such as within closures). That's because any reference saved to the
+  /* | Backbone gets attached to little app and thus can be used universally.
   /* \-------------------------------------------------------------------------------------
   *//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**////
 
@@ -29,34 +30,47 @@ class Backbone { // the Backbone class
   }
 
   boot() { // to boot and run the app
-    let init = () => { // init function
+    const $app = $(document), init = () => { // init function
       this.sessionActive = requests.loginCheck() // check to see if user is logged in
       console.log('Logged in: ', this.sessionActive);
-
     };
-
-    let creds = { first: "Daisy", last: "Duke", username: "DoubleDee", email: "daisy@duke.com", password: "boob" }
-    // let creds = { first: "Ikenna", last: "Ugwuh", username: "Kenny", email: "i@e.com", password: "pass" }
-    $(document) // register live event handlers (use jquery)
-      .on('click', '.register', function(){
-          // requests.register(JSON.stringify(creds))
-          requests.register(creds)
-               .then(resp => {console.log('resp: ', resp)}, err => console.log("error: ", err))
-
-      })
-      .on('click', '.login', function(){
-        requests.login(creds)
-        .then(resp => {console.log('resp: ', resp)}, err => console.log("error: ", err))
-      })
-      .ready(init); // when the document is ready, init
-
-    $(window).resize(() => { // when the window is resized
-
-    });
+    this.register($app).ready(init); // register and initialise the app
   }
 
   getRef(id) { // to get a previously saved DOM reference
     return this.refs[id];
+  }
+
+  deserialize(datastring) { // to turn a serialised string of data into a json object
+    let jso = {}, data = datastring.split('&');
+    return $.each(data, (key, value) => {
+      key = value.split('=')
+      jso[key[0]] = decodeURIComponent(key[1])
+    }), jso;
+  }
+
+  determineSize(pqtr) { // to get and set the size of the pqtr as a string
+    let w = Number(pqtr.width), h = Number(pqtr.height);
+    return pqtr.size = (w >= 1400 || h >= 1400) ? "HD" :
+      (w >= 900 || h >= 900) ? "large" : (w >= 600 || h >= 600) ? "medium" :
+      (w >= 250 || h >= 250) ? "small" : "tiny", pqtr;
+  }
+
+  register($interface) { // to register live event handlers on the app
+    let creds = { first: "Ikenna", last: "Ugwuh", username: "KennyKen", email: "i@e.com", password: "pass" }
+    return $interface
+      .on('click', '.register', () => {
+        requests.register(creds)
+          .then(resp => {console.log('resp: ', resp)}, err => console.log("error: ", err))
+      })
+      .on('click', '.login', () => {
+        console.log('logging in...')
+        requests.login(creds)
+          .then(data => this.saveSesh(data), err => console.log("error: ", err))
+      })
+      .on('submit', 'form', () => { return false })
+      .on('submit', '.addPqtr', function(){ app.upload($(this).serialize()) })
+      , $interface;
   }
 
   saveRef(id, R) { // to save a reference to the DOM
@@ -66,15 +80,43 @@ class Backbone { // the Backbone class
   saveSesh(data) { // to save the session once a user has loggedin
     let expires = new Date();
     expires.setTime(expires.getTime() + 1000 * 60 * 60 * 24 * 7); // set the expiry date for one week from now
+    cookies.set(access_token, data.access.token, { expires: expires }) // store the access token in a cookie
+    this.user = data.user; // set up the logged in user
     this.sessionActive = true; // activate the session
-    console.log(expires)
-    cookies.set(access_token, data.access_token, { expires: expires }) // store the access token in a cookie
-    delete data.access_token; // remove the access token from the response
-    this.user = data; // set up the logged in user
-    console.log(this.user)
+    console.log("User logged in: ", this.user)
+  }
+
+  upload(pqtr) { // to upload a pqtr to the database
+    pqtr = this.deserialize(pqtr); // get the object from the serialised string
+    console.log("grabbing pqtr...")
+    fetch(proxy + pqtr.source).then(_=>_.blob()).then(blob => { // grab a blob (the bytes) of the pqtr
+      console.log("determining size...")
+      const img = new Image(), // create an image
+            reader = new FileReader(), // create a file reader
+            upload = pqtr => requests.upload(pqtr) // define a callback to upload the pqtr
+              .then(resp => {
+                console.log("pqtr uploaded successfully.")
+                console.log('response: ', resp)
+                $('body').append($('<img>').attr("src", resp.blob)); // set the pqtr in the body (temporary)
+              }, err => console.log("error: ", err)) // error uploading pqtr
+      reader.readAsDataURL(blob); // turn the bytes into a base64 string of binary data
+      reader.addEventListener('loadend', e => { // once the bytes have been read
+        img.src = pqtr.blob = e.srcElement.result; // set the result as the blob of the pqtr and the source of the image
+        img.onload = () => { // once the source is set,  the image will load
+          // after loading is complete, determine the natural (actual) size of the image, then upload
+          pqtr.width = img.naturalWidth.toString();
+          pqtr.height = img.naturalHeight.toString();
+          upload(this.determineSize(pqtr));
+          console.log("uploading...")
+        };
+        img.onerror = () => { // catch any errors resolving the image
+          console.log("image error.")
+        };
+      });
+    })
   }
 };
 
-// construct and export little bone
-const bone = new Backbone();
-export default bone;
+// construct and export little app
+const app = new Backbone();
+export default app;
