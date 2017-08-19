eeafb9d64759afa2932a44af0d749ff57f1e9e61
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-19 08:25:11 -0400

    Update preparations.

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -24,9 +24,8 @@ extension Config {
     /// Add all models that should have their
     /// schemas prepared before the app boots
     private func setupPreparations() throws {
-        preparations.append(Post.self)
-        preparations.append(Pqtr.self)
         preparations.append(User.self)
         preparations.append(Token.self)
+        preparations.append(Pqtr.self)
     }
 }
