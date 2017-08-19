72ad528f67753430b6051c8b387d66eef84517af
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-19 08:21:20 -0400

    Edit class with description.

--- App/modules/cookies.js
+++ App/modules/cookies.js
@@ -1,33 +1,42 @@
 import cookie from 'cookie';
 import assign from 'object-assign';
 
-class Cookies {
-    get(name, options = {}) {
-        const cookies = cookie.parse(document.cookie);
-        return readCookie(cookies[name], options);
-    }
+class Cookies { // the Cookies class
+  /* |-------------------------------------------------------------------------------------
+  /* | This class is not a component. The Cookies class is in charge of getting and setting
+  /* | cookies on the browser. Cookies can be used to determine things about the session,
+  /* \ such as whether or not a user is logged in.
+  /* |
+  /* | This class is initialised before being exported and thus can be used universally, an-
+  /* | ywhere in the app, within any ES6 module, simply by importing it.
+  /* \-------------------------------------------------------------------------------------
+  *//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**////
 
-    getAll(options = {}) {
-        const cookies = cookie.parse(document.cookie);
-        const result = {};
-        for (let name in cookies) result[name] = readCookie(cookies[name], options);
-        return result;
-    }
+  get(name, options = {}) {
+    const cookies = cookie.parse(document.cookie);
+    return readCookie(cookies[name], options);
+  }
 
-    set(name, value, options) {
-        if (typeof value === 'object') value = JSON.stringify(value);
-        document.cookie = cookie.serialize(name, value, options);
-    }
+  getAll(options = {}) {
+    const cookies = cookie.parse(document.cookie), result = {};
+    for (let name in cookies) result[name] = readCookie(cookies[name], options);
+    return result;
+  }
 
-    remove(name, options) {
-        const finalOptions = (options = assign({
-            }, options, {
-                expires: new Date(1970, 1, 1, 0, 0, 1),
-                maxAge: 0
-            })
-        );
-        document.cookie = cookie.serialize(name, '', finalOptions);
-    }
+  set(name, value, options) {
+      if (typeof value === 'object') value = JSON.stringify(value);
+      document.cookie = cookie.serialize(name, value, options);
+  }
+
+  remove(name, options) {
+      const finalOptions = (options = assign({
+          }, options, {
+              expires: new Date(1970, 1, 1, 0, 0, 1),
+              maxAge: 0
+          })
+      );
+      document.cookie = cookie.serialize(name, '', finalOptions);
+  }
 }
 
 function isParsingCookie(value, doNotParse) {
--- App/modules/requests.js
+++ App/modules/requests.js
@@ -3,21 +3,36 @@ import Backbone from './backbone';
 import cookies from './cookies';
 import access_token from './constants/token';
 
-export default class Requests {
-    static login (creds, headers) { return !!headers? headers:
-      headers = { Authorization: "Basic " + btoa(creds.username + ':' + creds.password) },
-      console.log(headers), $.post({ url:  '/login',
-        headers: headers
-      })
-    }
+export default class Requests { // the Requests class
+  /* |-------------------------------------------------------------------------------------
+  /* | This class is not a component. These are our requests. The Requests class is in cha-
+  /* | rge of facilitating any and all communication with our server or apis.
+  /* |
+  /* | Though the Requests class can move around and be seen by other modules, since this
+  /* | class doesn't store any data, the methods are declared static and the class never
+  /* | actually has to be initialised. It can be used anywhere in the app, within any ES6
+  /* | module, simply by importing it.
+  /* \-------------------------------------------------------------------------------------
+  *//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**////
 
-    static loginCheck () { return cookies.get(access_token) ? true : false }
+  static login (creds, headers) { return !!headers? headers:
+    headers = { Authorization: "Basic " + btoa(creds.username + ':' + creds.password) },
+    $.post({ url:  '/login', headers: headers })
+  }
 
-    static logout () { cookies.remove(access_token); Backbone.sessionActive = false }
+  static upload (pqtr) {
+    return $.post({ url: "_", contentType: "application/json", data: JSON.stringify(pqtr),
+      headers: { Authorization: "Bearer " + cookies.get(access_token) }
+    })
+  }
 
-    static register (creds) { return $.post({ url: "/register",
-        contentType: 'application/json',
-        data: JSON.stringify(creds)
-      })
-    }
+  static loginCheck () { return cookies.get(access_token) ? true : false }
+
+  static logout () { cookies.remove(access_token); Backbone.sessionActive = false }
+
+  static register (creds) { return $.post({ url: "/register",
+      contentType: 'application/json',
+      data: JSON.stringify(creds)
+    })
+  }
 }
