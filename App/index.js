3af8514325a5fbf3ac1ef2b894c678c1861059c0
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-21 16:43:57 -0400

    Set up react router.

--- App/index.js
+++ App/index.js
@@ -1,11 +1,15 @@
 import Backbone from './modules/backbone'
 import React, { Component } from 'react'
-import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Link } from "react-router-dom"
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './index.scss'
+import TransitionGroup from 'react-transition-group/TransitionGroup'
 
 import Home from './components/home'
 import Single from './components/single'
+import Upload from './components/upload'
+import Login from './components/login'
+import Register from './components/register'
 
 class App extends Component {
   componentDidMount() { // the app is mounted, DOM nodes are ready
@@ -17,14 +21,20 @@ class App extends Component {
       <div className="App">
         <header>
           <nav className="nav-bar">
-            <span className="logo">Pqtrz.</span>
+            <Link to="/"><span className="logo">Pqtrz.</span></Link>
             <Link to="/">Home</Link>
             <Link to="/single">Single</Link>
+            <Link to="/upload">Upload</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
           </nav>
         </header>
-        <footer>&copy; 2017 | Pqtrz. No Rights Reserved. Yet.</footer>
           <Route exact path="/" component={Home} />
           <Route exact path="/single" component={Single} />
+          <Route exact path="/upload" component={Upload} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        <footer>&copy; 2017 | Pqtrz. No Rights Reserved. Yet.</footer>
       </div>
     )
   }
