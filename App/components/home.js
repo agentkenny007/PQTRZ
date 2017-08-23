f3568faf5c8c45f1b179f3bafca8fb63f9222da4
Ikenna Asad <ikennaugwuh@gmail.com>
2017-08-23 13:16:23 -0400

    Clean up.

--- App/components/home.js
+++ App/components/home.js
@@ -1,16 +1,18 @@
+import $ from 'jquery'
 import React, { Component } from 'react'
 import Container, { PqtrList } from './pqtr-container'
 import app from '../modules/backbone'
 
 export default class Home extends Component {
- render() {
-  return (
-   <page>
-     <Container
-       content={ <PqtrList pqtrz={app.pqtrz} /> }
-       ref={ container => app.saveRef('pqtr-container', container) }
-     />
-   </page>
-  )
- }
+  render() {
+    $('body').css("background", 'rgb(245, 247, 250)')
+    return (
+      <page>
+        <Container
+          content={ <PqtrList pqtrz={app.pqtrz} /> }
+          ref={ container => app.saveRef('pqtr-container', container) }
+        />
+      </page>
+    )
+  }
 }
